9a88f3b9a5417ddded2324ccd626a8f307f87096
metaironia <serezha364@gmail.com>
2024-03-04 04:28:12 +0300

    added button draw

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -20,10 +20,24 @@ GraphicsFuncStatus CrackProgramRun (void) {
                            background_image, 0, 0, txGetExtentX (background_image), txGetExtentY (background_image),
                            TX_NULL);
 
+    CrackButtonDraw (CRACK_WINDOW_SIZE_X * 2 / 3, CRACK_WINDOW_SIZE_Y * 1 / 3);
+    CrackButtonDraw (CRACK_WINDOW_SIZE_X * 2 / 3, CRACK_WINDOW_SIZE_Y * 2 / 3);
 
     txDeleteDC (background_image);
 
     return GRAPHICS_FUNC_STATUS_OK;
 }
 
+GraphicsFuncStatus CrackButtonDraw (const double x_topleft, const double y_topleft) {
 
+    if (!txSetColor (TX_white, BUTTON_BORDER_THICKNESS))
+        return GRAPHICS_FUNC_STATUS_FAIL;
+
+    if (!txSetFillColor (TX_ORANGE))
+        return GRAPHICS_FUNC_STATUS_FAIL;
+
+    if (!txRectangle (x_topleft, y_topleft, x_topleft + BUTTON_SIZE_X, y_topleft - BUTTON_SIZE_Y))
+        return GRAPHICS_FUNC_STATUS_FAIL;
+
+    return GRAPHICS_FUNC_STATUS_OK;
+}
--- src/graphics.h
+++ src/graphics.h
@@ -1,8 +1,6 @@
 #ifndef GRAPHICS_H
 #define GRAPHICS_H
 
-const int CRACK_WINDOW_SIZE_X = 600;
-const int CRACK_WINDOW_SIZE_Y = 400;
 
 enum GraphicsFuncStatus {
 
@@ -10,7 +8,16 @@ enum GraphicsFuncStatus {
     GRAPHICS_FUNC_STATUS_FAIL
 };
 
+const int CRACK_WINDOW_SIZE_X = 600;
+const int CRACK_WINDOW_SIZE_Y = 400;
+
+const double BUTTON_BORDER_THICKNESS = 3;
+const double BUTTON_SIZE_X           = 150;
+const double BUTTON_SIZE_Y           = 60;
+
 GraphicsFuncStatus CrackProgramRun (void);
+
+GraphicsFuncStatus CrackButtonDraw (const double x_topleft, const double y_topleft);
 
 
 #endif
