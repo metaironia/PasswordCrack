67d1ea1638ae302ac56575898c43658b945dcdcb
metaironia <serezha364@gmail.com>
2024-03-04 22:48:10 +0300

    updated rofls

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -21,13 +21,13 @@ GraphicsFuncStatus CrackProgramRun (void) {
                            background_image, 0, 0, txGetExtentX (background_image), txGetExtentY (background_image),
                            TX_NULL);
 
-    CrackButtonDraw (CRACK_WINDOW_SIZE_X * 2 / 3, CRACK_WINDOW_SIZE_Y * 1 / 3, "ПОЗВОНИТЬ В МВД");
+    CrackButtonDraw (CRACK_WINDOW_SIZE_X * 2 / 3, CRACK_WINDOW_SIZE_Y * 1 / 3, "ЗВЯКНУТЬ ДЕДУ");
     CrackButtonDraw (CRACK_WINDOW_SIZE_X * 2 / 3, CRACK_WINDOW_SIZE_Y * 2 / 3, "CRACK");
 
     while (1) {
 
         if (MouseInsideButtonCheck (CRACK_WINDOW_SIZE_X * 2 / 3, CRACK_WINDOW_SIZE_Y * 1 / 3))
-            txMessageBox ("Вызвали наряд на ваш адрес, ожидайте");
+            txMessageBox ("Дед умер, звякайте менторам");
 
         if (PressedButtonCheck (CRACK_WINDOW_SIZE_X * 2 / 3, CRACK_WINDOW_SIZE_Y * 2 / 3)) {
 
